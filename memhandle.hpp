c12bfdb5ea693aa99bc75522fd3a0fa988875687
Sfinktah Bungholio <sfinktah@github.spamtrak.org>
2017-02-04 22:24:23 +1100

    memhandle.hpp [time 22:24]

--- memhandle.hpp
+++ memhandle.hpp
@@ -7,10 +7,6 @@
  *
  */
 
-#include <cstdint>
-#include <cstring>
-#include <type_traits>
-
 class memBrick
 {
 protected:
@@ -33,90 +29,90 @@ public:
         : _handle(copy._handle)
     { }
 
-	static memBrick scan(memBrick base, std::size_t size, const char* pattern)
-	{
-		struct nibble
-		{
-			std::uint8_t value  = 0;
-			std::uint8_t offset = 0;
-		} nibbles[128];
+    static memBrick scan(memBrick base, std::size_t size, const char* pattern)
+    {
+        struct nibble
+        {
+            std::uint8_t value  = 0;
+            std::uint8_t offset = 0;
+        } nibbles[128];
 
-		std::size_t count = 0;
+        std::size_t count = 0;
 
-		for (; pattern; pattern = std::strpbrk(pattern, " "))
-		{
-			pattern += std::strspn(pattern, " "); // Discard whitespace
+        for (; pattern; pattern = std::strpbrk(pattern, " "))
+        {
+            pattern += std::strspn(pattern, " "); // Discard whitespace
 
-			if (pattern[0] != '?')
-			{
-				nibbles[count].value      = std::uint8_t(std::strtol(pattern, NULL, 16));
-				nibbles[count + 1].offset = nibbles[count].offset;
+            if (pattern[0] != '?')
+            {
+                nibbles[count].value      = std::uint8_t(std::strtol(pattern, NULL, 16));
+                nibbles[count + 1].offset = nibbles[count].offset;
 
-				count++;
-			}
+                count++;
+            }
 
-			nibbles[count].offset++;
-		}
+            nibbles[count].offset++;
+        }
 
-		for (std::size_t i = 0; i < size - nibbles[count].offset; i++)
-		{
-			memBrick currentOffset = base.offset(i);
+        for (std::size_t i = 0; i < size - nibbles[count].offset; i++)
+        {
+            memBrick currentOffset = base.offset(i);
 
-			bool found = true;
+            bool found = true;
 
-			for (std::size_t j = 0; j < count; ++j)
-			{
-				if (nibbles[j].value != currentOffset.offset(nibbles[j].offset).read<std::uint8_t>())
-				{
-					found = false;
+            for (std::size_t j = 0; j < count; ++j)
+            {
+                if (nibbles[j].value != currentOffset.offset(nibbles[j].offset).read<std::uint8_t>())
+                {
+                    found = false;
 
-					break;
-				}
-			}
+                    break;
+                }
+            }
 
-			if (found)
-			{
-				return currentOffset;
-			}
-		}
+            if (found)
+            {
+                return currentOffset;
+            }
+        }
 
-		return nullptr;
-	}
+        return nullptr;
+    }
 
-	static memBrick scan(MODULEINFO module, const char* pattern)
-	{
-		return memBrick::scan(module.lpBaseOfDll, module.SizeOfImage, pattern);
-	}
+    template <typename T>
+    std::enable_if_t<std::is_pointer<T>::value, T> as() const
+    {
+        return reinterpret_cast<T>(this->_handle);
+    }
 
-	template <typename T>
-	std::enable_if_t<std::is_pointer<T>::value, T> as() const
-	{
-		return reinterpret_cast<T>(this->_handle);
-	}
+    template <typename T>
+    std::enable_if_t<std::is_lvalue_reference<T>::value, T> as() const
+    {
+        return *reinterpret_cast<std::remove_reference_t<T>*>(this->_handle);
+    }
 
-	template <typename T>
-	std::enable_if_t<std::is_same<T, std::uintptr_t>::value, T> as() const
-	{
-		return reinterpret_cast<std::uintptr_t>(this->_handle);
-	}
+    template <typename T>
+    std::enable_if_t<std::is_same<T, std::uintptr_t>::value, T> as() const
+    {
+        return reinterpret_cast<std::uintptr_t>(this->_handle);
+    }
 
-	template <typename T>
-	std::enable_if_t<std::is_same<T, std::intptr_t>::value, T> as() const
-	{
-		return reinterpret_cast<std::intptr_t>(this->_handle);
-	}
+    template <typename T>
+    std::enable_if_t<std::is_same<T, std::intptr_t>::value, T> as() const
+    {
+        return reinterpret_cast<std::intptr_t>(this->_handle);
+    }
+
+    operator bool()
+    {
+        return (this->as<void*>() != nullptr);
+    }
 
     template <typename T>
     T read() const
     {
         return *this->as<T*>();
     }
-
-	template <typename T>
-	T& ref() const
-	{
-		return *this->as<T*>();
-	}
 
     memBrick save(memBrick& out) const
     {
